b63b990160613740146141ee00e6df2bf2ab1623
Shobir <33521457+ShobirIrsan@users.noreply.github.com>
2020-09-04 00:14:18 +0700

    program esp 8266

---
+++ tes_esp_wifi_ap_kom_http/tes_esp_wifi_ap_kom_http.ino
@@ -0,0 +1,32 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+IPAddress ip(192, 168, 11, 4);
+IPAddress gateway(192, 168, 11, 1);
+IPAddress subnet(255, 255, 255, 0);
+ESP8266WebServer server(80);
+char * ssid_ap = "ESP8266_SSID";
+char * password_ap = "sobir123";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(ip, gateway, subnet);
+  WiFi.softAP(ssid_ap, password_ap);
+  server.on("/dpot", insert);
+  server.begin();
+  Serial.println("data");
+}
+
+void loop() {
+  server.handleClient();
+}
+
+void insert() {
+  String dataKirim;
+  String desa;
+  desa = server.arg(0);
+  dataKirim = "sayang";
+  Serial.println(desa);
+  server.send(200, "text/html", dataKirim);
+  Serial.println("respon");
+}
